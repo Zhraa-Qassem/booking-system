51f0378af5ec7f1c4b02d2c18d1b95a49dd40707
zhraa <zhraa2005spn@gmail.com>
2023-10-03 19:55:28 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
   return (
     <div className="app">
     <NavBar/>
+
+    
       <div>
     <Routes>    
     <Route  path="/" element={<Home/>}></Route>    
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
-    <div>
-      home 
+    <div className='home'>
+      
+      <div className="header">
+        <div className="left">
+          <div>
+            <h1>Find your Route!</h1>
+          </div>
+          <div>
+            <h3>
+              Explore this route finding website
+              find the best route for your destination efficiently!
+            </h3>
+            <button>
+              Explore!
+            </button>
+          </div>
+        </div>
+        <div className="right">
+          <img src='' alt='' />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
